585ac7dde07a0d31795ab0952b4ecfbe5367a1bc
Mohit Kumar Mohanta <mohitmohanta1144@gmail.com>
2026-06-22 17:38:38 +0530

    Update authController.js

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -14,7 +14,7 @@ export const googleAuth = async (req, res) => {
     const token = jwt.sign({id:user._id}, process.env.SECRET_KEY, {expiresIn:"7d"})
     //cookie generate
     res.cookie("token", token,
-      {httpOnly:true, secure:true, sameSite: "none", maxAge: 7*24*60*60*1000}
+      {httpOnly:true, secure: true, sameSite: "none", maxAge: 7*24*60*60*1000}
     )
     return res.status(200).json(user)
 
